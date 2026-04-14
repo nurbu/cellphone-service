09f39c2970f810d870f27b75f3f9f8f2ec655ecb
Nurbu <lamanurbu2@gmail.com>
2026-04-14 15:09:40 -0400

    Used the getters and setter after creating a new instance of CellPhoneApplication for my newPhone.

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -18,15 +18,34 @@ public class CellPhoneApplication {
 
         System.out.print("What is the serial number? ");
         serialNumber = scanner.nextInt();
+        scanner.nextLine();
         System.out.print("What is the model? ");
         model = scanner.nextLine();
         System.out.print("Who is the carrier? ");
         carrier = scanner.nextLine();
         System.out.print("What is the phone number? ");
         phoneNumber = scanner.nextLine();
-        System.out.print("Who is the owner of the service? ");
+        System.out.print("Who is the owner of the service? \n");
         owner = scanner.nextLine();
 
-        
+        // Create a CellPhone instance called newPhone
+        CellPhone newPhone = new CellPhone();
+
+        //Used Setters to add the user's inputs.
+        newPhone.setSerialNumber(serialNumber);
+        newPhone.setModel(model);
+        newPhone.setCarrier(carrier);
+        newPhone.setPhoneNumber(phoneNumber);
+        newPhone.setOwner(owner);
+
+        System.out.println("My new phone information: ");
+
+        // Used Getters to output information about newPhone
+        System.out.println(newPhone.getSerialNumber());
+        System.out.println(newPhone.getModel());
+        System.out.println(newPhone.getCarrier());
+        System.out.println(newPhone.getPhoneNumber());
+        System.out.println(newPhone.getOwner());
+
     }
 }
