72ae0d82835de1932b19c1b1fc96ea656286d5b6
Nurbu <lamanurbu2@gmail.com>
2026-04-14 14:46:21 -0400

    Constructor setup

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
 public class CellPhone {
+    int serialNumber;
+    String model;
+    String carrier;
+    String phoneNumber;
+    String owner;
+
+    public CellPhone() {
+        serialNumber = 0;
+        model = "";
+        carrier = "";
+        phoneNumber = "";
+        owner = "";
+    }
+
+    
 }
